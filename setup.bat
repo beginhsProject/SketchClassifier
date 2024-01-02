8c76a098fe0bb7a3e728971f728c030130307359
beginhsProject <155469697+beginhsProject@users.noreply.github.com>
2024-01-02 19:32:39 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,10 +6,10 @@ pip install -r requirements.txt
 echo cd scripts > start_training.bat
 echo activate.bat >> start_training.bat
 echo cd .. >> start_training.bat
-echo start SketchClassifier.py "train" >> start_training.bat
+echo python SketchClassifier.py "train" >> start_training.bat
 echo cd scripts > start_testing.bat
 echo activate.bat >> start_testing.bat
 echo cd .. >> start_testing.bat
 echo set /p model=Enter model filename >> start_testing.bat
-echo start SketchClassifier.py "test" "%%model%%" >> start_testing.bat
+echo python SketchClassifier.py "test" "%%model%%" >> start_testing.bat
 del setup.bat
