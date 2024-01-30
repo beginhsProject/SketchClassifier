c0d1fcdc2fdb79dba840037fa3e98bffeb899d6b
beginhsProject <155469697+beginhsProject@users.noreply.github.com>
2024-01-30 19:46:20 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,15 +1,10 @@
-python -m venv "%cd%"
-cd scripts
-activate.bat
-cd ..
+python -m pip install virtualenv
+virtualenv sketch_env
+call sketch_env/scripts/activate.bat
 python -m pip install -r requirements.txt
-echo cd scripts > start_training.bat
-echo activate.bat >> start_training.bat
-echo cd .. >> start_training.bat
+echo call sketch_env/scripts/activate.bat > start_training.bat
 echo python SketchClassifier.py "train" >> start_training.bat
-echo cd scripts > start_testing.bat
-echo activate.bat >> start_testing.bat
-echo cd .. >> start_testing.bat
+echo call sketch_env/scripts/activate.bat > start_testing.bat
 echo set /p model=Enter model filename >> start_testing.bat
 echo python SketchClassifier.py "test" "%%model%%" >> start_testing.bat
 del setup.bat
