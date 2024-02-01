27c9ab4732896873a6a3eac89f1da860c02a5e7c
beginhsProject <155469697+beginhsProject@users.noreply.github.com>
2024-02-01 18:57:11 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,4 +7,7 @@ echo python SketchClassifier.py "train" >> start_training.bat
 echo call sketch_env/scripts/activate.bat > start_testing.bat
 echo set /p model=Enter model filename >> start_testing.bat
 echo python SketchClassifier.py "test" "%%model%%" >> start_testing.bat
+echo call sketch_env/scripts/activate.bat > download_data.bat
+echo python dataset_downloader.py >> download_data.bat
+echo pause >> download_data.bat
 del setup.bat
